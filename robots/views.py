51a2bc8c1b4f43ef027c4af33c393c94a53ce7f4
Georgypython <nov_georgy@inbox.ru>
2023-10-04 20:55:59 +0400

    add export excel view

--- robots/views.py
+++ robots/views.py
@@ -1,8 +1,10 @@
 from django.views import View
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from datetime import date, timedelta
+from openpyxl import Workbook
 from .models import Robot
 from .forms import ValidationForm
 import json
@@ -40,3 +42,20 @@ class PostRobotView(View):
         }
         return JsonResponse(data, status=201)
     
+    def export_to_excel(request):
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="robots.xlsx"'
+        wb = Workbook()
+        headers = ["Модель", "Версия", "Количество за неделю"]
+        today = date.today()
+        seven_day_before = today - timedelta(days=7)
+        robots = Robot.objects.filter(created__gte=seven_day_before).values('model', 'version').annotate(created_count=Count('id')).order_by('model', 'version')
+        for robot in robots:
+            if robot['model'] not in wb.sheetnames:
+                wb.create_sheet(title=robot['model'])
+                wb[robot['model']].append(headers)
+            wb[robot['model']].append([robot['model'], robot['version'], robot['created_count']])
+        if len(wb.sheetnames) > 1:
+            wb.remove(wb['Sheet'])
+        wb.save(response)
+        return response
